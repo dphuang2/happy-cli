1f9fcca409172e3a321f63da4f689a01170012f2
dphuang2 <dylan.p.huang@gmail.com>
2022-10-31 00:26:12 -0700

    ensure config setup

---
+++ src/util/ensure-config-setup.ts
@@ -0,0 +1,14 @@
+import * as os from 'node:os'
+import * as path from 'node:path'
+import * as fs from 'node:fs'
+
+const HAPPY_DIR_NAME = '.happy'
+const ARCHIVE_DIR_NAME = 'archive'
+
+export const HAPPY_DIR = path.join(os.homedir(), HAPPY_DIR_NAME)
+export const ARCHIVE_DIR = path.join(HAPPY_DIR, ARCHIVE_DIR_NAME)
+
+export const ensureConfigSetup = (): void => {
+  if (!fs.existsSync(ARCHIVE_DIR))
+    fs.mkdirSync(ARCHIVE_DIR, {recursive: true})
+}
--- src/util/generate-archive-path.ts
+++ src/util/generate-archive-path.ts
@@ -1,15 +1,6 @@
-import * as os from 'node:os'
-import * as path from 'node:path'
-import * as fs from 'node:fs'
-
-const HAPPY_DIR_NAME = '.happy'
-const ARCHIVE_DIR_NAME = 'archive'
-
-export const HAPPY_DIR = path.join(os.homedir(), HAPPY_DIR_NAME)
-export const ARCHIVE_DIR = path.join(HAPPY_DIR, ARCHIVE_DIR_NAME)
+import {ARCHIVE_DIR, ensureConfigSetup} from './ensure-config-setup'
 
 export const generateArchivePath = (): string => {
-  if (!fs.existsSync(ARCHIVE_DIR))
-    fs.mkdirSync(ARCHIVE_DIR, {recursive: true})
+  ensureConfigSetup()
   return `${ARCHIVE_DIR}/${new Date().toISOString()}.tgz`
 }
