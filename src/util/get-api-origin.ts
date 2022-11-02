eed6344f9deea1e798186d827cce96618423fcd1
dphuang2 <dylan.p.huang@gmail.com>
2022-11-02 03:03:44 -0700

    new domain

--- src/util/get-api-origin.ts
+++ src/util/get-api-origin.ts
@@ -1,4 +1,4 @@
 export const getApiOrigin = ({dev}: {dev: boolean}): string => {
   if (dev) return 'http://localhost:8911'
-  return 'https://happy-api-nkxm.onrender.com'
+  return 'https://dash.konfigthis.com'
 }
